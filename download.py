d24dbb10ed08766989120217a3dc75bff97f2baf
Abir <abirshukla@gmail.com>
2016-07-02 07:43:26 -0500

    Now Downloads Images and puts the images into seprate directory

--- download.py
+++ download.py
@@ -29,6 +29,21 @@ def crawlCSS(url):
 		return arr
 	except Exception,e:
 		print str(e)
+def crawlIMG(url):
+	try:
+		source_code=requests.get(url)
+		plain_text=source_code.text
+		soup=BeautifulSoup(plain_text)
+		arr = []
+		check = 0
+		for link in soup.findAll('img'):
+			href=link.get('src')
+			href_test=str(href)
+			if href_test.startswith("http") == False:
+				arr.append(href_test)
+		return arr
+	except Exception,e:
+		print str(e)	
 
 def crawlJS(url):
 	try:
@@ -58,11 +73,19 @@ def getReplaceLink(arr):
 	res = []
 	for link in arr:
 		index = link.rfind('/')
-		res.append(link[index+1:])
 		if link.endswith(".js"):
 			res.append("JS/"+link[index+1:])
 		else:
-			res.append("CSS/"+link[index+1:])
+			if link.endswith(".css"):
+				res.append("CSS/"+link[index+1:])
+			else:
+				res.append("IMG/"+link[index+1:])
+	return res
+def getStripLink(arr):
+	res = []
+	for link in arr:
+		index = link.rfind('/')
+		res.append(link[index+1:])
 	return res
 def getHTML(url):
 	source_code=requests.get(url)
@@ -70,13 +93,16 @@ def getHTML(url):
 	soup=BeautifulSoup(plain_text)	
 	return soup
 
-name = "http://ashobiz.asia/wrapbootstrap/boot-extended161/demo/"
+
+
+name = "http://abirshukla.pythonanywhere.com/"
 print "CLEARING WORKSPACE"
 if os.path.exists("CSS/"):
 	shutil.rmtree('CSS/')
 if os.path.exists("JS/"):
 	shutil.rmtree('JS/')
-
+if os.path.exists("IMG/"):
+	shutil.rmtree('IMG/')
 
 
 print "CSS FILES ---------"
@@ -111,22 +137,27 @@ for i in range(0,len(cssLinks)):
 		testfile.retrieve(cssLinks[i],updatedCss[i][4:])
 	except:
 		print "Error at "+cssLinks[i]
+
+os.chdir("../")
+os.makedirs("IMG")
+os.chdir("IMG/")
+print "GETTING IMAGES -----"
+originalIMG = crawlIMG(name)
+imgLinks = getLinks(originalIMG,name)
+replaceIMG = getReplaceLink(originalIMG)
+stripImg = getStripLink(originalIMG)
+for i in range(0,len(originalIMG)):
+	testfile = urllib.URLopener()
+	testfile.retrieve(imgLinks[i],stripImg[i])
+	code = code.replace(originalIMG[i],replaceIMG[i])
+
+
 os.chdir("../")
 f = open('webpage.html','w')
 f.write(str(code))
 f.close()
 
-"""
 
-f = open('test.html','w')
-f.write("test")
-f.close()
-os.chdir("../")
-f = open('test2.html','w')
-f.write("test2")
-f.close()
-
-"""
 
 
 
