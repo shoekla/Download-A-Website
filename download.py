463a7f44ed5613fe5e87c04a98e3262a9b364f15
Abir <abirshukla@gmail.com>
2016-07-02 07:49:06 -0500

    Added raw_input so user doesn't need to alter code just enter web url, also made 1 method to act as hub. All in all Version 1.0 of application (or just script in this case)

--- download.py
+++ download.py
@@ -94,70 +94,70 @@ def getHTML(url):
 	return soup
 
 
+def downloadWebsite(name):
+	print "CLEARING WORKSPACE"
+	if os.path.exists("CSS/"):
+		shutil.rmtree('CSS/')
+	if os.path.exists("JS/"):
+		shutil.rmtree('JS/')
+	if os.path.exists("IMG/"):
+		shutil.rmtree('IMG/')
 
-name = "http://abirshukla.pythonanywhere.com/"
-print "CLEARING WORKSPACE"
-if os.path.exists("CSS/"):
-	shutil.rmtree('CSS/')
-if os.path.exists("JS/"):
-	shutil.rmtree('JS/')
-if os.path.exists("IMG/"):
-	shutil.rmtree('IMG/')
 
+	print "CSS FILES ---------"
+	originalCSS = crawlCSS(name)
+	cssLinks = getLinks(originalCSS,name)
+	updatedCss = getReplaceLink(originalCSS)
+	print "JS FILES -----------"
+	originalJS = crawlJS(name)
+	jsLinks = getLinks(originalJS,name)
+	updatedJs = getReplaceLink(originalJS)
+	print "HTML CODE---------"
+	code = str(getHTML(name))
+	for i in range(0,len(originalCSS)):
+		code = code.replace(originalCSS[i],updatedCss[i])
+	for i in range(0,len(originalJS)):
+		code = code.replace(originalJS[i],updatedJs[i])
+	print "Aquiring files"
+	os.makedirs("JS")
+	os.chdir("JS/")
+	for i in range(0,len(jsLinks)):
+		try:
+			testfile = urllib.URLopener()
+			testfile.retrieve(jsLinks[i],updatedJs[i][3:])
+		except:
+			print "Error at "+jsLinks[i]
+	os.chdir("../")
+	os.makedirs("CSS")
+	os.chdir("CSS/")
+	for i in range(0,len(cssLinks)):
+		try:
+			testfile = urllib.URLopener()
+			testfile.retrieve(cssLinks[i],updatedCss[i][4:])
+		except:
+			print "Error at "+cssLinks[i]
 
-print "CSS FILES ---------"
-originalCSS = crawlCSS(name)
-cssLinks = getLinks(originalCSS,name)
-updatedCss = getReplaceLink(originalCSS)
-print "JS FILES -----------"
-originalJS = crawlJS(name)
-jsLinks = getLinks(originalJS,name)
-updatedJs = getReplaceLink(originalJS)
-print "HTML CODE---------"
-code = str(getHTML(name))
-for i in range(0,len(originalCSS)):
-	code = code.replace(originalCSS[i],updatedCss[i])
-for i in range(0,len(originalJS)):
-	code = code.replace(originalJS[i],updatedJs[i])
-print "Aquiring files"
-os.makedirs("JS")
-os.chdir("JS/")
-for i in range(0,len(jsLinks)):
-	try:
+	os.chdir("../")
+	os.makedirs("IMG")
+	os.chdir("IMG/")
+	print "GETTING IMAGES -----"
+	originalIMG = crawlIMG(name)
+	imgLinks = getLinks(originalIMG,name)
+	replaceIMG = getReplaceLink(originalIMG)
+	stripImg = getStripLink(originalIMG)
+	for i in range(0,len(originalIMG)):
 		testfile = urllib.URLopener()
-		testfile.retrieve(jsLinks[i],updatedJs[i][3:])
-	except:
-		print "Error at "+jsLinks[i]
-os.chdir("../")
-os.makedirs("CSS")
-os.chdir("CSS/")
-for i in range(0,len(cssLinks)):
-	try:
-		testfile = urllib.URLopener()
-		testfile.retrieve(cssLinks[i],updatedCss[i][4:])
-	except:
-		print "Error at "+cssLinks[i]
-
-os.chdir("../")
-os.makedirs("IMG")
-os.chdir("IMG/")
-print "GETTING IMAGES -----"
-originalIMG = crawlIMG(name)
-imgLinks = getLinks(originalIMG,name)
-replaceIMG = getReplaceLink(originalIMG)
-stripImg = getStripLink(originalIMG)
-for i in range(0,len(originalIMG)):
-	testfile = urllib.URLopener()
-	testfile.retrieve(imgLinks[i],stripImg[i])
-	code = code.replace(originalIMG[i],replaceIMG[i])
+		testfile.retrieve(imgLinks[i],stripImg[i])
+		code = code.replace(originalIMG[i],replaceIMG[i])
 
 
-os.chdir("../")
-f = open('webpage.html','w')
-f.write(str(code))
-f.close()
+	os.chdir("../")
+	f = open('webpage.html','w')
+	f.write(str(code))
+	f.close()
 
-
+name = raw_input("Enter Website you want downloaded: ")
+downloadWebsite(name)
 
 
 
